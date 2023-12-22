0949532c9c91152426f3a61bbba870ed82efa4b1
initialencounter <2911583893@qq.com>
2023-12-22 23:33:09 +0800

    chore: change to personality-plus

--- safe.js
+++ safe.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const personality = fs.readFileSync('./personality.json')
+const personality = fs.readFileSync('./personality-plus.json')
 
 const safe = personality.toString('base64')
 fs.writeFileSync('safe',safe)
